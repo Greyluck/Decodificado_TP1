b2a0943b08e8ffcfa350465f31513293b975f5b2
Greyluck <36477858+Greyluck@users.noreply.github.com>
2023-05-15 19:56:11 -0300

    Nuevas pruebas unitarias

--- Proyecto/UnitaryTesting.py
+++ Proyecto/UnitaryTesting.py
@@ -10,21 +10,22 @@ import Etapa5
 
 import doctest
 
-def show_letterboard_VerificationTest():
+#-----------------------------------------------------------------------------------------------------------------------
+def show_letterboard():
     """
     >>> Etapa1.show_letterboard(["a","b","c","d","e","f","g","h","i","j"])
     ['[A]', '[B]', '[C]', '[D]', '[E]', '[F]', '[G]', '[H]', '[I]', '[J]']
     """
 
-
+#-----------------------------------------------------------------------------------------------------------------------
 letters_in_board = '[A][B][C][E][D][E][F][G][H][I]'
-results_in_board = '[][][][][][][][][][]'
-success = 1
-mistake = 2
+results_in_board = '[ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]'
+success = 0
+mistake = 0
 letters_list = ['[A]', '[B]', '[C]', '[D]', '[E]', '[F]', '[G]', '[H]', '[I]', '[J]']
 turns = 0
-words = ["Arbol","Bosque","Camping","Descanso","Entorno","Flor","Gaviota","Higo","Intemperie","Jardin"]
-words_dict= {"Arbol":"Descripcion generica 1",
+words = ["Árbol","Bosque","Camping","Descanso","Entorno","Flor","Gaviota","Higo","Intemperie","Jardin"]
+words_dict= {"Árbol":"Descripcion generica 1",
             "Bosque":"Descripcion generica 2",
             "Camping":"Descripcion generica 3",
             "Descanso":"Descripcion generica 4",
@@ -34,19 +35,51 @@ words_dict= {"Arbol":"Descripcion generica 1",
             "Higo":"Descripcion generica 8",
             "Intemperie":"Descripcion generica 9",
             "Jardin":"Descripcion generica 10",}
-def print_board ():
+
+def print_board():
     # Se agrego blank line para que coincida con la forma mostrada en el PDF. 
     # Doctest revisa las lineas vacias usando ese formato
     """
     >>> Etapa1.print_board(letters_in_board, results_in_board, success, mistake, letters_list, turns, words, words_dict)
     [A][B][C][E][D][E][F][G][H][I]
-    [][][][][][][][][][]
+    [ ][ ][ ][ ][ ][ ][ ][ ][ ][ ]
     <BLANKLINE>
-    Aciertos: 1
-    Errores: 2
+    Aciertos: 0
+    Errores: 0
     Turno letra A - Palabra de 5 letras
     Definicion: Descripcion generica 1
     """
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+def validate_lenght_and_grammar():
+    """
+    >>> Etapa1.validate_lenght_and_grammar("Palabra", 7)
+    'Palabra'
+    """
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+actual_letter = "Á"
+correct_word = "Árbol"
+resultboard = ["b"]
+turns_description_list = ["Descripcion"]
+def add_answer():
+    """
+    >>> Etapa1.add_answer("Árbol", actual_letter, correct_word, resultboard, turns_description_list, turns, success, mistake)
+    Palabra correcta!
+    <BLANKLINE>
+    (1, 0)
+
+    >>> Etapa1.add_answer("Arboles", actual_letter, correct_word, resultboard, turns_description_list, turns, success, mistake)
+    Palabra incorrecta - La respuesta correcta era: Árbol
+    <BLANKLINE>
+    (0, 1)
+    """
+
+# TODO: Las pruebas unitarias no son completamente "Unitarias", ya que dependen del orden en el que son ejecutadas.
+# Para corregir esto se requiere implementar las variables dentro de las funciones. 
+# Eso sera agregado mas adelante. 
 
 print (doctest.testmod())
 
@@ -54,9 +87,9 @@ print (doctest.testmod())
 # #---------------------------------------------------------
 # Etapa1.show_letterboard(random_letters)                                                                               --> Verificado
 # Etapa1.print_board(letters_in_board, results_in_board, success, mistake, letters_list, turns, words, words_dict)      --> Verificado
-# Etapa1.ask_for_word()
-# Etapa1.validate_lenght_and_grammar(string, lenght) 
-# Etapa1.add_answer(word, actual_letter, correct_word, resultboard, turns_description_list, turns, success, mistake)
+# Etapa1.ask_for_word()                                                                                                 --> Ignorado
+# Etapa1.validate_lenght_and_grammar(string, lenght)                                                                    --> Verificado
+# Etapa1.add_answer(word, actual_letter, correct_word, resultboard, turns_description_list, turns, success, mistake)    --> Verificado
 # Etapa1.run_match(words_dict, words, random_letters)
 # Etapa1.main_etapa1()
 # Etapa1.run_match(words_dict, words, random_letters)
